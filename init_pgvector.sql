9a4d5d5e56ca734e5f73834be8f5187190552d94
Gourav Singh Bais <gouravbais08@gmail.com>
2024-09-19 22:42:52 +0530

    Update init_pgvector.sql

--- init_pgvector.sql
+++ init_pgvector.sql
@@ -1,11 +1,11 @@
 
--- Install the extension we just compiled
+-- Install the extension you just compiled
 
 CREATE EXTENSION IF NOT EXISTS vector;
 
 /*
-"768" dimensions for our vector embedding are critical - that is the
-number of dimensions of our open source embeddings model output, for later in the
+"768" dimensions for your vector embedding are critical; that is the
+number of dimensions of your open source embeddings model output, for later in the
 article.
 */
 
